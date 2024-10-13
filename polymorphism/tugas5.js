ffa37c165b477ae5ec05bcba5b1abe7b02699707
clarissamulya <clarissamulya@upi.edu>
2024-10-13 12:40:44 +0700

    Add files via upload

--- polymorphism/tugas5.js
+++ polymorphism/tugas5.js
@@ -1,4 +1,4 @@
-class Sepeda {
+class Sepeda { 
     constructor(nama, merk, tahun, jenis) {
         this.nama = nama;
         this.merk = merk;
@@ -33,14 +33,41 @@ class SepedaBalap extends Sepeda {
     }
 }
 
-//polimorfisme
+class SepedaListrik extends Sepeda {
+    constructor(nama, merk, tahun, jenis, kapasitasBaterai) {
+        super(nama, merk, tahun, jenis);
+        this.kapasitasBaterai = kapasitasBaterai; 
+    }
+
+    infoSepeda() {
+        return `${super.infoSepeda()} Sepeda ini memiliki kapasitas baterai ${this.kapasitasBaterai} mAh.`;
+    }
+}
+
+class SepedaBMX extends Sepeda {
+    constructor(nama, merk, tahun, jenis, ukuranRoda) {
+        super(nama, merk, tahun, jenis);
+        this.ukuranRoda = ukuranRoda; 
+    }
+
+    infoSepeda() {
+        return `${super.infoSepeda()} Sepeda ini memiliki ukuran roda ${this.ukuranRoda} inci.`;
+    }
+}
+
+// Polimorfisme
 const sepedaLipat = new Sepeda("Foldy", "Polygon", 2020, "lipat");
 const sepedaGunung1 = new SepedaGunung("MountainPro", "Trek", 2022, "gunung", "depan dan belakang");
 const sepedaBalap1 = new SepedaBalap("Speedster", "Specialized", 2021, "balap", 50);
+const sepedaListrik1 = new SepedaListrik("Electro", "Xiaomi", 2023, "listrik", 10000);
+const sepedaBMX1 = new SepedaBMX("StuntPro", "GT", 2019, "BMX", 20);
 
-const sepedaList = [sepedaLipat, sepedaGunung1, sepedaBalap1];
+const sepedaList = [sepedaLipat, sepedaGunung1, sepedaBalap1, sepedaListrik1, sepedaBMX1];
 
 // Menampilkan informasi dengan polimorfisme
 sepedaList.forEach(sepeda => {
     console.log(sepeda.infoSepeda());
 });
+
+
+
